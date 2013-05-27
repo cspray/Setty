2b45576be1e593b7c95caf208c605db81d0f4683
Charles Sprayberry <cspray@gmail.com>
2013-05-27 13:13:54 -0400

    interface that represents a class creating appropriate EnumValues

---
+++ src/Setty/Enum.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Interface that represents an object that is used by calling code to create the
+ * appropriate typehintable objects storing the enum's values.
+ * 
+ * @author Charles Sprayberry
+ * @license See LICENSE in source root
+ * @version 0.1.0
+ * @since 0.1.0
+ */
+
+namespace Setty;
+
+/**
+ * The primary purpose of this interface is to require the appropriate magic method
+ * functionality for \Setty\Enum implementations.
+ */
+interface Enum {
+
+    /**
+     * Should return an instance of \Setty\EnumValue that corresponds to the
+     * blueprint for the \Setty\Enum.
+     *
+     * The $name of the static method called should correspond to a constant name
+     * set in the blueprint of the Enum. No $arguments should be supported; this
+     * parameter is present as the __callStatic() magic method is expecting its
+     * presence. Implementations should not support using this parameter and passing
+     * arguments to the magic static method is undefined behavior.
+     *
+     * @param string $name
+     * @param array $arguments
+     * @return \Setty\EnumValue
+     *
+     * @throw \Setty\Exception\EnumNotFoundException
+     */
+    public static function __callStatic($name, $arguments);
+
+}
