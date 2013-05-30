dc5d4195ae37ff03e789ff8304947fdb4054385f
Charles Sprayberry <cspray@gmail.com>
2013-05-30 12:57:50 -0400

    adding example for usage

---
+++ examples/compass/index.php
@@ -0,0 +1,21 @@
+<?php
+
+$rootDir = \dirname(\dirname(__DIR__));
+
+require_once $rootDir . '/src/ClassLoader/Loader.php';
+
+$Loader = new \ClassLoader\Loader();
+
+$Loader->registerNamespaceDirectory('Setty', $rootDir . '/src');
+$Loader->setAutoloader();
+
+$ValueBuilder = new \Setty\Builder\SettyEnumValueBuilder();
+$EnumBuilder = new \Setty\Builder\SettyEnumBuilder($ValueBuilder);
+
+$EnumBuilder->storeFromArray(['name' => 'Compass', 'constant' => ['NORTH' => 'n', 'SOUTH' => 's', 'EAST' => 'e', 'WEST' => 'w']]);
+$Enum = $EnumBuilder->buildStored('Compass');
+
+var_dump($Enum::NORTH());
+var_dump($Enum::SOUTH());
+var_dump($Enum::EAST());
+var_dump($Enum::WEST());
